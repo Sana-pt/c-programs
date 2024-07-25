a122e4afb0d0e484744fed4b4067416d0f110fa3
Sana-pt <sanasadik39@gamil.com>
2024-07-25 23:49:00 +0530

    third commit

--- quadratic/main.c
+++ quadratic/main.c
@@ -8,29 +8,37 @@ Welcome to GDB Online.
 *******************************************************************************/
 #include <stdio.h>
 #include<math.h>
-int main()
+
+void quadratic(float a,float b,float c,float d)
 {
-    float a,b,c,img,d,r1,r2;
-    printf("Enter the coefficients of quadratic equation:\n");
-    scanf("%f %f %f",&a,&b,&c);
-    d=b*b-4*a*c;
+    float r1,r2,img;
     if(d>0)
     {
         r1=(-1*b+sqrt(d))/(2*a);
-         r2=(-b-sqrt(d))/(2*a);
-         printf("Roots are:%.2f and %.2f",r1,r2);
+         r2=(-1*b-sqrt(d))/(2*a);
+         printf("Roots are:%.0f and %.0f",r1,r2);
     }
     else if(d==0)
     {
         r1=(-1*b)/(2*a);
-        printf("\nOnly one root %.2f",r1);
+        printf("\nOnly one root %.0f",r1);
         
     }
     else
     {
         r1=-1*b/(2*a);
         img=sqrt(-1*d)/(2*a);
-        printf("Roots are %.2f +%.2fi and %.2f -%.2fi",r1,img,r1,img);
+        printf("Roots are %.0f +%.0fi and %.0f -%.0fi",r1,img,r1,img);
     }
-    return 0;
 }
+    int main()
+        {
+         float a,b,c,d; 
+         printf("Enter the coefficients of quadratic equation:\n");
+         scanf("%f %f %f",&a,&b,&c);
+         d=b*b-4*a*c;
+           quadratic(a, b, c, d);
+          return 0;
+        }
+        
+   
